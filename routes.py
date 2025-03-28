d4d6515a2363bba52826106cee25fe1121590aa9
Krystian <krychac8@gmail.com>
2025-03-28 13:58:59 +0100

    chest_info graph attempts

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 import json
 from prices_data import cases, get_latest_price_from_file
+from flask import jsonify
 
 main = Blueprint('main', __name__)
 
@@ -20,4 +21,23 @@ def home():
 @main.route("/chest_info/<string:case_code>")
 def chest_info(case_code):
     chest_info = cases.get(case_code)
-    return render_template('chest_info.html', chest_info=chest_info)
+    return render_template('chest_info.html', chest_info=chest_info, case_code=case_code)
+
+
+@main.route("/get_price_history/<case_code>")
+def get_price_history(case_code):
+    try:
+        with open("prices.json", "r") as f:
+            data = json.load(f)
+
+        # Znajdź kod odpowiadający case_code
+        for chest_name, chest_info in cases.items():
+            if chest_info["code"] == case_code:
+                if case_code in data:
+                    return jsonify(data[case_code])
+                else:
+                    return jsonify([])  # Pusta lista, jeśli nie ma danych
+
+        return jsonify([])  # Jeśli kod nie istnieje
+    except Exception as e:
+        return jsonify({"error": str(e)})
